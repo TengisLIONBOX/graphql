de4515590806e155544cc6675996311c3996dbd6
Тэнгис Ариунболд <HP236500@G302-CC0145.local>
2024-03-15 17:36:28 +0800

    first

--- src/services/todo-service.ts
+++ src/services/todo-service.ts
@@ -1,28 +1,10 @@
+import { Todo, TodoCreateInput, TodoUpdateInput } from "@/graphql/generated";
 import { GraphQLError } from "graphql";
 import { nanoid } from "nanoid";
-
-export type Todo = {
-  id: string;
-  title: string;
-  completed: boolean;
-};
-
-export type TodoCreateInput = {
-  title: string;
-  completed: boolean;
-};
-
-export type TodoUpdateInput = {
-  id: string;
-  title: string;
-  completed: boolean;
-};
 
 const todoList: Todo[] = [
   { id: nanoid(), title: "Learn Next.js", completed: false },
 ];
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const getTodoList = async (): Promise<Todo[]> => {
   return todoList;
@@ -37,8 +19,8 @@ export const createTodo = (input: TodoCreateInput): Todo => {
 export const updateTodo = (input: TodoUpdateInput): Todo | undefined => {
   const todo = todoList.find((todo) => todo.id === input.id);
   if (!todo) throw new GraphQLError("Todo not found");
-  todo.title = input.title;
-  todo.completed = input.completed;
+  todo.title = `${input.title}`;
+  todo.completed = Boolean(input.completed);
   return todo;
 };
 
