2116a82fd445a2c51ef39b8d232a972357b20dde
Тэнгис Ариунболд <HP236500@G302-CC0145.local>
2024-04-11 16:59:27 +0800

    aaaaaaa

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   async headers() {
     return [
       {
+        // matching all API routes
         source: "/api/(.*)",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE" },
           {
             key: "Access-Control-Allow-Headers",
             value: "Content-Type, Authorization",
