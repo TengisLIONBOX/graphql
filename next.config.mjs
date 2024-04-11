447710c95d63ccf7491e63dfa84ce373ff76b750
Тэнгис Ариунболд <HP236500@G302-CC0145.local>
2024-04-11 16:51:12 +0800

    aa

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,32 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/(.*)",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              " X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "Content-Type, Authorization",
           },
         ],
       },
     ];
+  },
+  reactStrictMode: false,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL || "",
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "",
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || "",
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "",
   },
 };
 
